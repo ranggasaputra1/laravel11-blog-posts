9743dbf4488e55e989bd634100d95b638758656c
ranggasaputra1 <ranggasaputra10092003@gmail.com>
2024-04-19 13:12:56 +0700

    Add dashboard pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Spatie\FlareClient\View;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
 });
