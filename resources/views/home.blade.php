036e9dc87a7830d2fd455517fc902fd7fc1a26f6
ranggasaputra1 <ranggasaputra10092003@gmail.com>
2024-05-05 15:35:33 +0700

    Learn how to deploy project to web hosting

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,10 @@
     <x-slot:title>{{ $title }}</x-slot:title>
     <x-slot:header>{{ $header }}</x-slot:header>
 
+    {{-- Belajar --}}
+    {{-- Hari ini saya berhasil belajar untuk mendeploy project ke vercel, 000 webhost dan infinity free --}}
+    {{-- End Belajar --}}
+
     {{-- isi dari main --}}
     <h2>coba ini adalah bagian main dari halaman home</h2>
     {{-- end main --}}
