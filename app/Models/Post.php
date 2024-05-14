85bedd8be48e363053386788b0a9cc9a08b0d13b
ranggasaputra1 <ranggasaputra10092003@gmail.com>
2024-05-14 13:09:04 +0700

    Add Messassigment for allow to insert data from models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,11 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-Use Illuminate\Support\Arr;
 
 class Post extends Model
 {
     use HasFactory;
-
     protected $guarded = [];
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -9,7 +9,7 @@
         <h2 class="mb-3 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
         </a>
         <div class="text-base text-gray-500">
-            <a href="#">{{ $post['author'] }}</a> | 5 Mei 2024
+            <a href="#">{{ $post['author'] }}</a> | {{ $post->created_at->diffForHumans() }}
         </div>
         <p class="my-1 font-light">
             {{ $post['body'] }}
