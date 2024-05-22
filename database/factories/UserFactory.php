9f494c1663146cd35c14aeb9bd12bd541910a449
ranggasaputra1 <ranggasaputra10092003@gmail.com>
2024-05-22 11:19:18 +0700

    Add database factory for Post Models

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -41,12 +41,5 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
-
-    public function admin(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'is_admin' => true,
-        ]);
-    }
     
 }
