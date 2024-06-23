3485630d1c23adf43244fca2dc59b9de5c391297
ranggasaputra1 <ranggasaputra10092003@gmail.com>
2024-06-23 15:26:07 +0700

    fix: UiUx for single posts pages

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,24 +7,6 @@
         <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
 
             @foreach ($posts as $post)
-                {{-- <article class="py-5 lg:text-left sm:text-auto text-center max-w-screen-md">
-            <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
-                <h2 class="mb-3 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
-            </a>
-            <div> By
-                <a href="/authors/{{ $post->author->username }}"
-                    class="hover:underline text-base text-gray-500">{{ $post->author->name }}</a> in
-                <a href="/categories/{{ $post->category->slug }}"
-                    class="hover:underline text-base text-gray-500">{{ $post->category->name }}</a> |
-                {{ $post->created_at->diffForHumans() }}
-            </div>
-            <p class="my-1 font-light">
-                {{ Str::limit($post['body'], '150') }}
-            </p>
-            <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more
-                &raquo;</a>
-        </article> --}}
-
                 <article
                     class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
                     <div class="flex justify-between items-center mb-5 text-gray-500">
@@ -36,25 +18,25 @@
                         </a>
                         <span class="text-sm">{{ $post->created_at->diffForHumans() }}</span>
                     </div>
-                    <a href="/posts/{{ $post->slug }}">
+                    <a href="/posts/{{ $post->slug }}" class="hover:underline">
                         <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                             {{ $post->title }}
                         </h2>
                     </a>
-                    <p class="mb-5 font-light text-gray-500 dark:text-gray-400">Static websites are now used to
-                        bootstrap lots of websites and are becoming the basis for a variety of tools that even
-                        influence both web designers and developers influence both web designers and developers.</p>
+                    <p class="mb-5 font-light text-gray-500 dark:text-gray-400">{{ Str::limit($post->body), 150 }}</p>
                     <div class="flex justify-between items-center">
-                        <div class="flex items-center space-x-4">
-                            <img class="w-7 h-7 rounded-full"
-                                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
-                                alt="Jese Leos avatar" />
-                            <span class="font-medium dark:text-white">
-                                Jese Leos
-                            </span>
-                        </div>
-                        <a href="#"
-                            class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
+                        <a href="/authors/{{ $post->author->username }}">
+                            <div class="flex items-center space-x-3">
+                                <img class="w-7 h-7 rounded-full"
+                                    src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
+                                    alt="{{ $post->author->name }}" />
+                                <span class="font-medium text-sm dark:text-white">
+                                    {{ $post->author->name }}
+                                </span>
+                            </div>
+                        </a>
+                        <a href="/posts/{{ $post->slug }}"
+                            class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline text-sm">
                             Read more
                             <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
                                 xmlns="http://www.w3.org/2000/svg">
@@ -66,9 +48,7 @@
                     </div>
                 </article>
             @endforeach
-
         </div>
     </div>
-
     {{-- end main --}}
 </x-layout>
