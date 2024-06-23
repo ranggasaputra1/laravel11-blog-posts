7e2913d951fd5e525a417d91c95e72a6443731f9
ranggasaputra1 <ranggasaputra10092003@gmail.com>
2024-06-23 14:54:25 +0700

    fix: UiUx for all posts pages

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -16,14 +16,22 @@ class CategorySeeder extends Seeder
         Category::create([
             'name'=>'Web Programming',
             'slug'=>'Web-Programming',
+            'color' => 'red',
         ]);
         Category::create([
             'name'=>'Web Design',
             'slug'=>'Web-Design',
+            'color' => 'green',
         ]);
         Category::create([
             'name'=>'Personal',
             'slug'=>'personal',
+            'color' => 'blue',
+        ]);
+        Category::create([
+            'name'=>'UI UX',
+            'slug'=>'ui-ux',
+            'color' => 'yellow',
         ]);
     }
 }
